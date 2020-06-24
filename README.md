9e7ac2a399e362ccca0f1a9f7ed2f5defb649393
Chinseok Lee <me@askcompany.kr>
2020-06-24 12:36:33 +0000

    GrpcTokenizer 생성자에 dic_type 지원 추가

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # cola-komoran-python-client
 
+## 주의
+
+> 라이브러리를 업데이트되면, ray.remote 객체 재생성을 위해 ray cluster stop/start가 필요할 수도 있습니다.
+
 ## 라이브러리 설치
 
 ```sh
 pip install https://github.com/euclidsoft/cola-komoran-python-client/archive/master.zip
 ```
 
-## 샘플 코드
+## ray를 활용한 bulk 호출 샘플 코드
 
 ```python
 from cola_komoran_python_client import ray_init, ray_shutdown, summarize_batch_with_ray
@@ -18,5 +22,20 @@ ray_init()  # 처음 1회만 수행해주세요.
 keyword_list = summarize_batch_with_ray(sentence_list_series)
 
 ray_shutdown()  # 모든 작업이 끝내거나, ray cluster를 초기화시키고자 할 때
+```
+
+## 단일 호출 샘플 코드
+
+```python
+from cola_komoran_python_client import GrpcTokenizer
+
+tokenize = GrpcTokenizer("localhost:50051")
+summarizer = KeywordSummarizer(
+    tokenize = tokenize,
+    window = -1,
+    verbose = False,
+)
+summarizer.summarize(sentence_list, topk=10)
+
 ```
 
--- cola_komoran_python_client/__init__.py
+++ cola_komoran_python_client/__init__.py
@@ -12,52 +12,54 @@ from textrank import KeywordSummarizer
 def to_iterator(obj_ids):
     while obj_ids:
         done, obj_ids = ray.wait(obj_ids)
-        yield ray.get(done[0])
+        for obj_id in done:
+            yield ray.get(obj_id)
 
 
 class GrpcTokenizer:
-    def __init__(self, target):
+    def __init__(self, target, dic_type):
         channel = insecure_channel(target)
         self.stub = KomoranStub(channel)
+        self.dic_type = dic_type
 
-    def __call__(self, sentence, dic_type=0):
-        request = TokenizeRequest(dicType=dic_type, sentence=sentence)
+    def __call__(self, sentence):
+        request = TokenizeRequest(dicType=self.dic_type, sentence=sentence)
         response = self.stub.tokenize(request)
         keyword_list = response.keyword
         return keyword_list
 
 
-@ray.remote
-def summarize(sentence_list, target, window, verbose, topk):
-    tokenize = GrpcTokenizer(target)
-    summarizer = KeywordSummarizer(
-        tokenize = tokenize,
-        window = window,
-        verbose = verbose,
-    )
-    try:
-        return summarizer.summarize(sentence_list, topk=topk)
-    except ValueError:
-        return []
-
-
-def ray_init():
-    ray.init(address="auto")
+def ray_init(address="auto"):
+    ray.init(address=address)
 
 
 def ray_shutdown():
     ray.shutdown()
 
 
-def summarize_batch_with_ray(sentence_list_series, with_tqdm=True, target='localhost:50051', window=-1, verbose=False, topk=10):
+def summarize_batch_with_ray(sentence_list_series, dic_type=0, target='localhost:50051', verbose=False, with_tqdm=True):
     obj_ids = [
-        summarize.remote(sentence_list, target=target, window=window, verbose=verbose, topk=topk)
+        summarize.remote(sentence_list, target=target, verbose=verbose, dic_type=dic_type)
         for sentence_list in sentence_list_series
     ]
     if with_tqdm:
         return list(tqdm(to_iterator(obj_ids), total=len(obj_ids)))
     else:
         return ray.get(obj_ids)
+
+
+@ray.remote
+def summarize(sentence_list, target, verbose, dic_type=0):
+    tokenize = GrpcTokenizer(target, dic_type)
+    summarizer = KeywordSummarizer(
+        tokenize = tokenize,
+        window = -1,
+        verbose = verbose,
+    )
+    try:
+        return summarizer.summarize(sentence_list, topk=10)
+    except ValueError:
+        return []
 
 
 __call__ = ["ray_init", "ray_shutdown", "summarize_batch_with_ray"]
