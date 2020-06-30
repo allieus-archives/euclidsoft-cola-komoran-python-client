dd431c0dca126fcdf2b43312f2ef0ae48c3c189d
Chinseok Lee <me@askcompany.kr>
2020-06-30 13:30:39 +0900

    README.md open 함수 호출에 encoding 인자 추가

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ dependency_links = [
  
 setup(
     name='cola-komoran-python-client',
-    version='1.2.0',
+    version='1.2.1',
     url='https://github.com/euclidsoft/cola-komoran-python-client',
     author='Chinseok Lee',
     author_email='me@askcompany.kr',
     description='Python Client for cola-komoran',
     packages=find_packages(exclude=['tests']),
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding="utf8").read(),
     zip_safe=False,
     setup_requires=setup_requires,
     install_requires=install_requires,
