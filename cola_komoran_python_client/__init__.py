219f41f033d99c2bb47f795c83aae91e2d0d1ab5
Chinseok Lee <me@askcompany.kr>
2020-07-23 13:40:55 +0900

    14일전 커밋의 legacy summarize_without_ray 함수 제거

--- cola_komoran_python_client/__init__.py
+++ cola_komoran_python_client/__init__.py
@@ -96,17 +96,6 @@ def summarize_batch_with_ray(sentence_list_series, with_tqdm=True,
     else:
         return ray.get(obj_ids)
 
-def summarize_without_ray(sentence_list, target, window, verbose, topk, dic_type):
-    tokenize = GrpcTokenizer(target, dic_type=dic_type)
-    summarizer = KeywordSummarizer(
-        tokenize = tokenize,
-        window = window,
-        verbose = verbose,
-    )
-    try:
-        return summarizer.summarize(sentence_list, topk=topk)
-    except ValueError:
-        return []
 
 def ray_init(address="auto"):
     ray.init(address=address)
